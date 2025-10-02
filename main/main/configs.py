d7329e03efaaeac04d11f44a5792e29641f3766f
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-10-02 13:36:48 +0530

    Added configs and JSON config files

---
+++ main/main/configs.py
@@ -0,0 +1,25 @@
+STREAM_DISABLED= ["discod", "cli-no-stream"]
+
+ERROR_TOKEN = "__ERROR__"
+
+DEFAULT_PROMPT: str = r"""
+[ Your Default or fallback system prompt ]
+"""
+
+CHAT_PROMPT = r"""
+[ Your prompt for the chat model ]
+"""
+
+
+ROUTER_PROMPT = r"""
+[ Your prompt for the router. You may need to tweak the roting code logic to make it work ]
+"""
+
+
+CoT_PROMPT:str = r'''
+[ Your prompt for the reasoning / Chain of Thought (CoT) model ]
+'''
+
+CHAOS_PROMPT = r"""
+[ Optional: Alter ego of your assistant. MAKE IT CHAOTIC! ]
+"""
