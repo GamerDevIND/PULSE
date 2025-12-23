f43b07ecf4a78a35eae9bfdebe3a9ffe3e448d88
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-12-23 13:38:54 +0530

    Add Event_Manager to module exports

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,6 @@
 from .AI import AI
 from .models import Model
 from .utils import log
+from .Events import Event_Manager
 
-__all__ = ["AI", "Model", "log"]
+__all__ = ["AI", "Model", "log", "Event_Manager"]
