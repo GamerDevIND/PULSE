4285a7c5f1e159e5fbb477a4f727436bac2cce3b
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-02-08 16:48:48 +0530

    Update router.py

--- main/router.py
+++ main/router.py
@@ -72,7 +72,7 @@ class Router:
             return query, "cot"
         elif query.startswith(f"{self.manual_prefix}chaos"):
             query = query.removeprefix(f"{self.manual_prefix}chaos").strip()
-            return query, "chaos"
+            return query, "chat"
         elif query.startswith(f"{self.manual_prefix}vision"):
             query = query.removeprefix(f"{self.manual_prefix}vision").strip()
  
