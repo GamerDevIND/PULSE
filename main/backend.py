2dfecc03e8f1200bed9eea1f81c78c361c729190
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-03-14 13:17:02 +0530

    Update backend.py

--- main/backend.py
+++ main/backend.py
@@ -4,6 +4,8 @@ import asyncio
 from .generation_session import GenerationSession, DONE, FAIL
 from .tools import ToolRegistry
 from .utils import log
+from .configs import ERROR_TOKEN
+import inspect
 
 class Backend:
     def __init__(self, models_list_path, system_prompts, default_system_prompt) -> None:
@@ -15,7 +17,9 @@ class Backend:
         self.running_tasks = set()
 
     async def cancel_generation(self, session_id:str):
-        session = self.sessions[session_id]
+        session = self.sessions.get(session_id)
+        if not session: 
+            return
         await session.cancel()
 
     def get_session(self, session_id:str):
@@ -25,14 +29,15 @@ class Backend:
     def get_model(self, role):
         model_obj = self.models.get(role)
         return model_obj
-    
+
     def _init(self):
         t = asyncio.create_task(self._check_sessions())
         self.running_tasks.add(t)
 
     async def remove_session(self, session_id):
         await self.cancel_generation(session_id)
-        del self.sessions[session_id]
+        if session_id in self.sessions: 
+            del self.sessions[session_id]
 
     async def _check_sessions(self):
         while True:
@@ -45,28 +50,74 @@ class Backend:
 
     async def create_session(self, query:str, context:list[dict], tools_regis:ToolRegistry, role, system_prompt_override: str | None = None, 
                 options: dict | None = None, format_: dict | None = None, max_turns = 10, abs_max_turns = 50, regen_consent_callback= None):
-        
+
         model_obj = self.models.get(role)
 
         if not model_obj:
             await log(f"{role} not found in the model registry!","error")
             raise
-        
+
         session = GenerationSession(query, context, tools_regis, model_obj, 
                 system_prompt_override, options, format_, max_turns, abs_max_turns, regen_consent_callback)
-        
+
         session_id = session.id
-        
+
         self.sessions[session_id] = session
         return session_id, session
 
     async def close_sessions(self):
         for s in self.sessions.values():
             await s.cancel()
-        
+
         for task in list(self.running_tasks):
             task.cancel()
             try:
                 await task
             except asyncio.CancelledError:
                 pass
+
+
+class Generation:
+    def __init__(self, session:GenerationSession, remove_callback, append_callback, stream, user_save_prefix, think, 
+                 file_path, video_frames_mod, save_thinking) -> None:
+
+        '''
+        Note: `remove_callback` MUST cancel the given session through `session_id` before removal.
+        '''
+
+        self.session = session
+        self.session_id = session.id
+        self.remove_callback = remove_callback
+        self.append_callback = append_callback
+        self.stream_ = stream
+        self.user_save_prefix = user_save_prefix
+        self.think = think
+        self.file_path = file_path
+        self.video_frames_mod = video_frames_mod
+        self.save_thinking = save_thinking
+
+
+    async def stream(self, ):
+        try:
+            async for (thinking, content) in self.session.generate(self.stream_, self.user_save_prefix, self.think, 
+                                                                   self.file_path, self.video_frames_mod, self.save_thinking):
+
+                if content == ERROR_TOKEN:
+                    return
+
+                yield (thinking or "", content or "")
+
+        finally:
+            c = await self.session.get_context()
+            r = self.remove_callback(self.session_id)
+            if inspect.isawaitable(r):
+                await r
+
+            r = self.append_callback(c)
+            if inspect.isawaitable(r):
+                await r
+
+    async def terminate(self):
+        r = self.remove_callback(self.session_id)
+        if inspect.isawaitable(r):
+            await r
