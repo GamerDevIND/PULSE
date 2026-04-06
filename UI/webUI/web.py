17c64ecb2d2e5f3b61be8900f078acae03ef2c4a
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-04-06 22:54:45 +0530

    Update web.py

--- UI/webUI/web.py
+++ UI/webUI/web.py
@@ -13,7 +13,7 @@ from main.utils import log
 from main.configs import USERNAME
 
 app = Quart(__name__)
-ai = AI("main/openrouter_models_configs.json", mode='openrouter')
+ai = AI("main/Models_configs.json", mode='openrouter')
 
 def get_greeting():
     now = datetime.datetime.now()
