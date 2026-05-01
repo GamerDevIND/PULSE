6a20e0425fdf2bd9c28004ce929bf9f55e4ba003
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2026-05-01 23:37:44 +0530

    Update simple_terminal.py

--- UI/simple_terminal.py
+++ UI/simple_terminal.py
@@ -12,7 +12,7 @@ from main.utils import log, estimate_tokens
 colorama.init()
 
 async def main():
-    ai = AI("main/openrouter_models_configs.json", mode='openrouter', use_RAG=False)
+    ai = AI("main/Models_configs.json", mode='openrouter', use_RAG=False)
     await ai.init("cli")
 
     gen = None
